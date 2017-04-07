57c3fcf88d100d0b67f55d7aa3f59bcd982e77e3
Amy Parent <amy@amyparent.com>
2017-04-07 14:12:19 +0100

    Add tests results

--- data/02-ditl/cfg.c
+++
@@ -1,8 +0,0 @@
-// FCORE Configuration File - FCORE Test 1
-// Generated on 2017-03-29 by fcore_config.rb
-#include "fcore.h"
-
-const FCPayload fcore_payloads[FCORE_PAYLOAD_COUNT] = {
-    {10, "zero-mass spectrometer", 2, STATUS_BOOT},
-    {20, "thermometer", 1, STATUS_BOOT},
-};
